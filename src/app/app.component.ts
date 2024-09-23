43afa6ef4538d2d873b5180b56302010f18c891e
LuisLi18 <lrlitang18@gmail.com>
2024-09-23 12:35:51 -0500

    prueba 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+// app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testing';
+  responseData: any;
+
+  constructor(private http: HttpClient) {}
+
+  // Método para realizar la llamada POST
+  sendPostRequest() {
+    const apiUrl = 'http://agrotech.koreacentral.cloudapp.azure.com:8080/rice-crops'; // Cambia por tu URL de API
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const data = { key: 'value' }; // Cambia los datos según sea necesario
+
+    this.http.get(apiUrl).subscribe(
+      response => {
+        this.responseData = response;
+        console.log('Respuesta del servidor:', response);
+      },
+      error => {
+        console.error('Error al enviar los datos', error);
+      }
+    );
+  }
 }
